fc01a5ba7161351bafb399a1ea186f9122c80477
Bug-programmer <jalaludeen.n@yahoo.com>
2023-08-13 16:44:48 +0530

    Added Start Game

--- src/api/AirtableAPI.js
+++ src/api/AirtableAPI.js
@@ -1,129 +1,83 @@
-const Airtable = require('airtable');
-const { json } = require( 'express' );
+const Airtable = require("airtable");
+const { json } = require("express");
 const apiKey = process.env.AIRTABLE_API_KEY;
 const baseId = process.env.BASE_ID;
 const base = new Airtable({ apiKey }).base(baseId);
-const fs = require('fs');
-const path = require('path'); 
+const fs = require("fs");
+const path = require("path");
 
-
-
-
-const createRecord = async (data,Table) => {
-try {
-  const createdRecord = await base(Table).create(data);
-  return createdRecord;
-} catch (error) {
-  console.log(error)
-  throw error; // Throw the error to be caught by the caller
-}
+const createRecord = async (data, Table) => {
+  try {
+    const createdRecord = await base(Table).create(data);
+    return createdRecord;
+  } catch (error) {
+    console.log(error);
+    throw error; // Throw the error to be caught by the caller
+  }
 };
+
 const fetchGameData = async () => {
   try {
     const records = await base("Games")
       .select({
-        fields: ['GameID', 'GameName'],
+        fields: ["GameID", "GameName"],
       })
       .all();
-    const gameData = records.map(record => ({
-      id: record.get('GameID'),
-      name: record.get('GameName'),
+    const gameData = records.map((record) => ({
+      id: record.get("GameID"),
+      name: record.get("GameName"),
     }));
     return gameData;
   } catch (error) {
-    console.error('Error:', error);
+    console.error("Error:", error);
     return null;
   }
 };
 
-
 const getFile = (pdfArray, name) => {
-for (const file of pdfArray) {
-  if (file.originalname === name) {
-    const uploadsPath = path.join(__dirname, './../../uploads'); // Adjust the path as needed
-    const filePath = path.join(uploadsPath, name);
-    fs.writeFileSync(filePath, file.buffer);
-    return filePath;
-  }
-}
-return null;
-};
-
-
-const formatData = (pdf, data) => {
-const RecordForGameTable = generateDataForGame(data,"Name");
-const parsedData = getParseData(data);
-const formattedData = parsedData.map((Data, dataIndex) => {
-  const pdfFile = getFile(pdf, `${dataObject.Role}_Level${dataIndex+1}.pdf`); 
-  // Replace 'test' with the actual filename
-  const UniqueCode = generateUniqueCode(7);
-  if (pdfFile) {
-    return {
-      fields: {
-        GameID:UniqueCode,
-        GameName: Data.GameName,
-        GoogleSheet: Data.GoogleSheet,
-        NumberOfRounds: 1,
-        ResultsSubbmision: Data.ResultsSubbmision,
-        ScoreVisibilityForPlayers: Data.ScoreVisibility,
-        RolesAutoSelection : Data.RoleSelection,
-        IndividualInstructionsPerRound : Data.IndividualInstructions
-      },
-    };
+  for (const file of pdfArray) {
+    if (file.originalname === name) {
+      const uploadsPath = path.join(__dirname, "./../../uploads"); // Adjust the path as needed
+      const filePath = path.join(uploadsPath, name);
+      fs.writeFileSync(filePath, file.buffer);
+      return filePath;
+    }
   }
   return null;
-}).filter(Boolean); // Remove any null entries
-return formattedData;
 };
 
-const formatDataOriginal = (pdf, data) => {
-const parsedData = getParseData(data);
-const formattedData = parsedData.map((dataObject, dataIndex) => {
-  const pdfFile = getFile(pdf, 'test'); // Replace 'test' with the actual filename
-  if (pdfFile) {
-    return {
-      fields: {
-        GameID: dataObject.GameID,
-        Role: dataObject.Role,
-        LevelDescription: pdfFile,
-        Level: dataIndex + 1,
-      },
-    };
-  }
-  return null;
-}).filter(Boolean); // Remove any null entries
-return formattedData;
+const formatedDataformatGameData = (data, GameId) => {
+  return {
+    GameID: GameId,
+    GameName: data.GameName,
+    GoogleSheet: data.GoogleSheet,
+    NumberOfRounds: parseInt(data.NumberOfRounds),
+    ResultsSubbmision: data.ResultsSubbmision,
+    ScoreVisibilityForPlayers: data.ScoreVisibility,
+    RolesAutoSelection: data.RoleSelection,
+    IndividualInstructionsPerRound: data.IndividualInstructions,
+  };
 };
 
-
-const formatGameData = (data,GameId) => {
-    return {
-        GameID:GameId,
-        GameName: data.GameName,
-        GoogleSheet: data.GoogleSheet,
-        NumberOfRounds: parseInt(data.NumberOfRounds),
-        ResultsSubbmision: data.ResultsSubbmision,
-        ScoreVisibilityForPlayers: data.ScoreVisibility,
-        RolesAutoSelection : data.RoleSelection,
-        IndividualInstructionsPerRound : data.IndividualInstructions
-      
-  }
+const formatRoleData = (data, GameId, LevelDescription) => {
+  const records = data.map((datum) => ({
+    fields: {
+      GameID: GameId, // Update with the actual GameID
+      Role: JSON.parse(datum).role,
+      Submit: JSON.parse(datum).checked,
+      Description: LevelDescription,
+    },
+  }));
+  return records;
 };
 
-const formatRoleData = (data,GameId,LevelDescription) => {
-const records = data.map((datum) => (
- {
-  fields  :{
-    GameID: GameId, // Update with the actual GameID
-    Role: JSON.parse(datum).role,
-    Submit: JSON.parse(datum).checked,
-    Description : LevelDescription
-  }
-}));
-return records;
-};
-
-const formatLevelData = (data, GameId, pdfArray, NumberOfRounds, IndividualInstructionsPerRound) => {
+const formatLevelData = (
+  data,
+  GameId,
+  pdfArray,
+  NumberOfRounds,
+  IndividualInstructionsPerRound,
+) => {
   const records = data.map((datum) => {
     const parsedDatum = JSON.parse(datum);
     const role = parsedDatum.role;
@@ -136,8 +90,8 @@ const formatLevelData = (data, GameId, pdfArray, NumberOfRounds, IndividualInstr
             GameID: GameId,
             Role: role,
             Level: i,
-            LevelDescription: getFile(pdfArray, `${role}_Level${i}.pdf`)
-          }
+            LevelDescription: getFile(pdfArray, `${role}_Level${i}.pdf`),
+          },
         });
       }
       return levelRecords;
@@ -147,8 +101,8 @@ const formatLevelData = (data, GameId, pdfArray, NumberOfRounds, IndividualInstr
           GameID: GameId,
           Role: role,
           Level: 0,
-          LevelDescription: "No"
-        }
+          LevelDescription: "No",
+        },
       };
     }
   });
@@ -157,55 +111,67 @@ const formatLevelData = (data, GameId, pdfArray, NumberOfRounds, IndividualInstr
   return records.flat();
 };
 
-  
-
-
-const getParseData = (data) => {
-
-const parsedData = data.map((dataString) => {
-  return JSON.parse(dataString);
-});
-return parsedData;
+const startGame = async (data) => {
+  try {
+    const formatedData = {
+      GameID: data.gameId,
+      RoomNumber: data.roomNumber,
+      TotalPlayers: parseInt(data.numberOfPlayers),
+      GroupSize: parseInt(data.playersPerGroup),
+      TotalGrops: parseInt(data.numbersOfGroups),
+    };
+    await createRecord(formatedData, "GameInitiated");
+    console.log("Data successfully sent to Airtable");
+  } catch (error) {
+    console.error("Error creating game:", error);
+  }
 };
 
-const createGame = async (pdf, data,roles) => {
-try {
-  let LevelDescription = ""
-  const UniqueCode = generateUniqueCode(7);
-  const GameData = formatGameData(JSON.parse(data),UniqueCode);
-  if(!GameData.IndividualInstructionsPerRound){
-    LevelDescription = getFile(pdf,"individualPdf.pdf")
+const createGame = async (pdf, data, roles) => {
+  try {
+    let LevelDescription = "";
+    const UniqueCode = generateUniqueCode(7);
+    const GameData = formatGameData(JSON.parse(data), UniqueCode);
+    if (!GameData.IndividualInstructionsPerRound) {
+      LevelDescription = getFile(pdf, "individualPdf.pdf");
+    }
+    const RoleData = formatRoleData(roles, UniqueCode, LevelDescription);
+    const LevelData = formatLevelData(
+      roles,
+      UniqueCode,
+      pdf,
+      GameData.NumberOfRounds,
+      GameData.IndividualInstructionsPerRound,
+    );
+    await createRecord(GameData, "Games");
+    await createRecord(RoleData, "Role");
+    await createRecord(LevelData, "Level");
+    console.log("Data successfully sent to Airtable");
+  } catch (error) {
+    console.error("Error creating game:", error);
   }
-  const RoleData = formatRoleData(roles,UniqueCode,LevelDescription)
-  const LevelData = formatLevelData(roles,UniqueCode,pdf,GameData.NumberOfRounds,GameData.IndividualInstructionsPerRound)
-  await createRecord(GameData,"Games");
-  await createRecord(RoleData,"Role");
-  await createRecord(LevelData,"Level")
-  console.log('Data successfully sent to Airtable', output);
-} catch (error) {
-  console.error('Error creating game:', error);
-}
 };
 
 function generateUniqueCode(length) {
-const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
-let code = '';
+  const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+  let code = "";
 
-for (let i = 0; i < length; i++) {
-  const randomIndex = Math.floor(Math.random() * characters.length);
-  code += characters[randomIndex];
-}
+  for (let i = 0; i < length; i++) {
+    const randomIndex = Math.floor(Math.random() * characters.length);
+    code += characters[randomIndex];
+  }
 
-const timestamp = Date.now().toString(36).toUpperCase(); // Convert timestamp to base36 and uppercase
-code = timestamp + code;
+  const timestamp = Date.now().toString(36).toUpperCase(); // Convert timestamp to base36 and uppercase
+  code = timestamp + code;
 
-return code.substring(0, length); // Return only the desired length
+  return code.substring(0, length); // Return only the desired length
 }
 
 // Generate a code of length 7
 
 // Export the functions
 module.exports = {
-createGame,
-fetchGameData,
+  createGame,
+  fetchGameData,
+  startGame,
 };
--- src/app.js
+++ src/app.js
@@ -1,53 +1,68 @@
 // app.js
-require('dotenv').config({ path: __dirname + '/.env' });
+
+// Load environment variables from .env file
+require("dotenv").config({ path: __dirname + "/.env" });
+
 // Import required modules
-const express = require('express');
-const cors = require('cors');
-const app = express();
-const {createGame,fetchGameData} = require('./api/AirtableAPI'); 
-const multer = require('multer');
-// const upload = multer(); //
-const storage = multer.memoryStorage(); // Store files in memory as buffers
+const express = require("express");
+const cors = require("cors");
+const { createGame, fetchGameData, startGame } = require("./api/AirtableAPI");
+const multer = require("multer");
+
+// Set up multer storage for storing files in memory
+const storage = multer.memoryStorage();
 const upload = multer({ storage });
 
-// 
+// Create the Express app
+const app = express();
 
+// Use CORS middleware to handle cross-origin issues
 app.use(cors());
+
+// Parse JSON data from requests
 app.use(express.json());
 
-app.post('/new-game', upload.array('pdf'), async (req, res) => {
-try {
-  
-  createGame(req.files,req.body.data,req.body.roles)
-  res.status(200).json({ message: 'Data and PDFs submitted successfully' });
-} catch (error) {
-  console.error('Error:', error);
-  res.status(500).json({ error: 'An error occurred' });
-}
+// Define routes
+
+// Route for creating a new game
+app.post("/new-game", upload.array("pdf"), async (req, res) => {
+  try {
+    // Call createGame function with request data
+    createGame(req.files, req.body.data, req.body.roles);
+    res.status(200).json({ message: "Data and PDFs submitted successfully" });
+  } catch (error) {
+    console.error("Error:", error);
+    res.status(500).json({ error: "An error occurred" });
+  }
 });
 
-app.post('/start-game', async (req, res) => {
+// Route for starting a game
+app.post("/start-game", async (req, res) => {
   try {
-    
-    res.status(200).json({ message: 'Game Started successfully' });
+    // Call startGame function with request data
+    startGame(JSON.parse(req.body.data));
+    res.status(200).json({ message: "Game Started successfully" });
   } catch (error) {
-    console.error('Error:', error);
-    res.status(500).json({ error: 'An error occurred' });
+    console.error("Error:", error);
+    res.status(500).json({ error: "An error occurred" });
   }
-  });
-app.get('/games', async (req, res) => {
+});
+
+// Route for fetching game data
+app.get("/games", async (req, res) => {
   try {
-    const data = await fetchGameData()
-    res.status(200).json({data:data, message: 'Data and PDFs submitted successfully' });
+    const data = await fetchGameData();
+    res
+      .status(200)
+      .json({ data: data, message: "Data and PDFs submitted successfully" });
   } catch (error) {
-    console.error('Error:', error);
-    res.status(500).json({ error: 'An error occurred' });
+    console.error("Error:", error);
+    res.status(500).json({ error: "An error occurred" });
   }
-  });
-  
+});
 
 // Start the server
 const port = 3001; // Replace with your desired port number
 app.listen(port, () => {
-console.log(`Server running on port ${port}`);
+  console.log(`Server running on port ${port}`);
 });
