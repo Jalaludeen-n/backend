015e800af5e78d0002922fe72425d41e825466aa
Bug-programmer <jalaludeen.n@yahoo.com>
2023-11-21 17:19:51 +0530

    Refactor

--- src/components/role.js
+++ src/components/role.js
@@ -1,4 +1,9 @@
+const { updateGameInitiatedRecord } = require("../controller/airtable");
 const { getFile } = require("../helpers/helper");
+const {
+  assignRoleManually,
+  getRemainingRoles,
+} = require("./level/gameDetails");
 
 const getRolePdf = async (data) => {
   try {
@@ -15,6 +20,48 @@ const getRolePdf = async (data) => {
   }
 };
 
+const getRoles = async (data) => {
+  try {
+    roles = await getRemainingRoles(data.roomNumber, data.groupName);
+
+    return {
+      success: true,
+      data: roles,
+      message: "Roles Fetched",
+    };
+  } catch (error) {
+    console.error("Error getting roles", error);
+    throw error;
+  }
+};
+
+const selectRole = async (data) => {
+  try {
+    assignRoleManually(data.groupName, data.email, data.role, data.roomNumber);
+    let filed = ["RoomNumber", "GroupName", "ParticipantEmail"];
+    let condition = `AND({RoomNumber} = "${data.roomNumber}",{ParticipantEmail} = "${data.email}")`;
+    let response = await fetchWithCondition("Participant", condition, filed);
+    const formattedData = {
+      Role: data.role,
+    };
+
+    await updateGameInitiatedRecord(
+      "Participant",
+      response[0].id,
+      formattedData,
+    );
+    return {
+      success: true,
+      message: "Role updated",
+    };
+  } catch (error) {
+    console.error("Error selecting role:", error);
+    throw error;
+  }
+};
+
 module.exports = {
   getRolePdf,
+  selectRole,
+  getRoles,
 };
