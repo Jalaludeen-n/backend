cfb360a8d8923c8f8f3e780823aad2d67de713b6
Bug-programmer <jalaludeen.n@yahoo.com>
2023-11-23 10:12:47 +0530

    Fix: roles

--- src/components/level/gameDetails.js
+++ src/components/level/gameDetails.js
@@ -1,8 +1,8 @@
 const { fetchWithCondition } = require("../../controller/airtable");
 
 const assignedRoles = {}; // { groupName: { email: role } }
-let roles = [];
-let duplicateRoles = [];
+let roles = {};
+let duplicateRoles = {};
 
 const emailExistsInAssignedRoles = (email) => {
   for (const group in assignedRoles) {
@@ -28,7 +28,7 @@ const assignRoleToGroup = (groupName, email, roomNumber) => {
   }
 
   const groupAssignedRoles = Object.keys(assignedRoles[roomNumber][groupName]);
-  for (const role of roles) {
+  for (const role of roles[roomNumber]) {
     if (!groupAssignedRoles.includes(role)) {
       if (!assignedRoles[roomNumber][groupName][role]) {
         assignedRoles[roomNumber][groupName][role] = [];
@@ -38,7 +38,7 @@ const assignRoleToGroup = (groupName, email, roomNumber) => {
     }
   }
 
-  for (const dupRole of duplicateRoles) {
+  for (const dupRole of duplicateRoles[roomNumber]) {
     if (
       assignedRoles[roomNumber][groupName][dupRole] &&
       assignedRoles[roomNumber][groupName][dupRole].length === 1
@@ -59,15 +59,22 @@ const fetchGameID = async (roomNumber) => {
   return response[0].fields.GameID;
 };
 
-const fetchRolesFromAirtable = async (gameID) => {
+const fetchRolesFromAirtable = async (gameID, roomNumber) => {
   const condition = `{GameID} = "${gameID}"`;
   const fields = ["Role", "Submit", "Duplicate"];
   const airtableRoles = await fetchWithCondition("Role", condition, fields);
 
+  if (!roles[roomNumber]) {
+    roles[roomNumber] = [];
+  }
+  if (!duplicateRoles[roomNumber]) {
+    duplicateRoles[roomNumber] = [];
+  }
+
   airtableRoles.forEach((data) => {
-    roles.push(data.fields.Role);
+    roles[roomNumber].push(data.fields.Role);
     if (data.fields.Duplicate) {
-      duplicateRoles.push(data.fields.Role);
+      duplicateRoles[roomNumber].push(data.fields.Role);
     }
   });
 };
@@ -78,10 +85,10 @@ const getRole = async (data) => {
   const roomNumber = data.roomNumber;
 
   if (!emailExistsInAssignedRoles(email)) {
-    if (!roles.length) {
+    if (!roles[roomNumber]) {
       const gameID = await fetchGameID(data.roomNumber);
 
-      await fetchRolesFromAirtable(gameID);
+      await fetchRolesFromAirtable(gameID,roomNumber);
 
       const roleAssigned = assignRoleToGroup(groupName, email, roomNumber);
       return roleAssigned;
@@ -96,25 +103,25 @@ const getRole = async (data) => {
 };
 
 const getRemainingRoles = async (roomNumber, groupName) => {
-  if (!roles.length) {
+  if (!roles[roomNumber]) {
     const gameID = await fetchGameID(roomNumber);
-    await fetchRolesFromAirtable(gameID);
+    await fetchRolesFromAirtable(gameID,roomNumber);
   }
 
   if (!assignedRoles[roomNumber]) {
-    return roles;
+    return roles[roomNumber];
   }
 
   if (!assignedRoles[roomNumber][groupName]) {
-    return roles;
+    return roles[roomNumber];
   }
 
-  const unassignedRoles = roles.filter((role) => {
+  const unassignedRoles = roles[roomNumber].filter((role) => {
     return !Object.keys(assignedRoles[roomNumber][groupName]).includes(role);
   });
 
   if (unassignedRoles.length === 0) {
-    const unassignedDupRoles = duplicateRoles.filter((role) => {
+    const unassignedDupRoles = duplicateRoles[roomNumber].filter((role) => {
       return (
         !Object.keys(assignedRoles[roomNumber][groupName]).includes(role) ||
         assignedRoles[roomNumber][groupName][role].length === 1
